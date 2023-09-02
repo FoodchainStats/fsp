0ba0563b1dfbb0b5bec887a551dd2cc97e49b01e
David Lee <david@lee269.co.uk>
2023-09-02 15:28:58 +0100

    acquire ag workforce function

--- R/acquire.R
+++ R/acquire.R
@@ -64,3 +64,34 @@ acquire_ct <- function(path){
   
   
 }
+
+
+
+acquire_ag_workforce <- function(path) {
+  
+  if (!missing(path)) {
+    if (!dir.exists(path)) 
+      stop(paste(path, "does not exist"))
+  }
+  
+  url <- url_ag_workforce()
+  html <- rvest::read_html(url)
+  links <- html |> rvest::html_elements("a")
+  linktext <- links |> rvest::html_text2()
+  linkurls <- links |> rvest::html_attr("href")
+  datalink <- which(stringr::str_detect(linktext, "Agricultural workforce in the United Kingdom at 1 June"))
+  
+  filename <- linkurls[datalink]
+  
+  if (missing(path)) {
+    tmp <- tempfile()
+    utils::download.file(filename, tmp)
+    ag_workforce_file <- tmp
+  } else {
+    utils::download.file(filename, destfile = paste0(path, "/", 
+                                                "ag_workforce.ods"))
+    ag_workforce_file <- paste0(path, "/", "ag_workforce.ods")
+  }
+  
+  return(ag_workforce_file)
+}
