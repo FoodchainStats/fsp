2a8e01f96ea1cfbb3bfb88b1d183c48aed5cabe6
David Lee <david@lee269.co.uk>
2023-09-03 19:03:56 +0100

    Updated documentation

--- R/urls.R
+++ R/urls.R
@@ -1,10 +1,10 @@
 #' URL for the current JOBS03 dataset
 #'
-#' @return a url for JOBS03
+#' @return The url for JOBS03.
 #' @export
 #'
 #' @examples
-#' 
+#' url_jobs03()
 url_jobs03 <- function() {
   url <- "https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/employeejobsbyindustryjobs03"
   
@@ -25,10 +25,11 @@ url_jobs03 <- function() {
 
 #' URL for the current Consumer Trends dataset
 #'
-#' @return
+#' @return The url for the dataset.
 #' @export
 #'
 #' @examples
+#' url_ct()
 url_ct <- function() {
   url <- "https://www.ons.gov.uk/file?uri=/economy/nationalaccounts/satelliteaccounts/datasets/consumertrends/current/ct.csv"
   return(url)
@@ -39,11 +40,12 @@ url_ct <- function() {
 #' URL for Defra Agricultural Workforce data
 #'
 #' @return The url for the landing page that contains the dataset.
-#'   get_jobs03_data() uses this function and some webscraping to download and
+#'   [get_jobs03_data()] uses this function and some webscraping to download and
 #'   extract thte actual data file.
 #' @export
 #'
 #' @examples
+#' url_ag_workforce()
 url_ag_workforce <- function() {
   url <- "https://www.gov.uk/government/statistical-data-sets/agricultural-workforce-in-the-united-kingdom-at-1-june"
   return(url)
