b00586acb307648324734a6638630d0406b76e1c
David Lee <david@lee269.co.uk>
2023-10-29 17:22:18 +0000

    ggplot helper

---
+++ R/helpers.R
@@ -0,0 +1,6 @@
+ignore_unused_imports <- function() {
+  # To get rid of NOTE about use of ggplot in vignette
+  # https://r-pkgs.org/dependencies-in-practice.html#how-to-not-use-a-package-in-imports
+  
+  ggplot2::ggplot()
+}
