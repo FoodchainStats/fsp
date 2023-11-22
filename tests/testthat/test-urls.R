faf44c23c0713e1c7cb9244a7d183e585ca58966
David Lee <david.lee@defra.gov.uk>
2023-11-22 09:17:39 +0000

    test url_*

---
+++ tests/testthat/test-urls.R
@@ -0,0 +1,22 @@
+test_that("url_jobs03 returns an xls", {
+  url <- url_jobs03()
+  
+  expect_equal(stringr::str_sub(url, -3, -1), "xls")
+  
+})
+
+
+test_that("url_ag_workforce returns an ods", {
+  url <- url_ag_workforce()
+  
+  expect_equal(stringr::str_sub(url, -3, -1), "ods")
+  
+})
+
+
+test_that("url_unctad returns a 7z", {
+  url <- url_unctad()
+  
+  expect_equal(stringr::str_sub(url, -2, -1), "7z")
+  
+})
